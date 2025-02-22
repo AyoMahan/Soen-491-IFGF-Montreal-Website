24f2ffc0c56afa7a97b9839b9b8f328f8feae0ac
Mahanaim Rubin Yo <108913637+AyoMahan@users.noreply.github.com>
2025-02-22 00:42:54 -0500

    nav bar #11

--- client/IFGF-MONTREAL/src/App.vue
+++ client/IFGF-MONTREAL/src/App.vue
@@ -1,23 +1,16 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar
+  }
+};
 </script>
 
 <template>
-  <header>
-    <img alt="IFGF Logo" class="logo" src="@/assets/logo-ifgf.png" width="125" height="125" />
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/events">Events</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
 <style scoped>
-
 </style>
