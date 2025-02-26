ad573443140c2934a689700de871761c60d24fa1
Mahanaim Rubin Yo <108913637+AyoMahan@users.noreply.github.com>
2025-02-26 00:43:17 -0500

    Connect page #13 #16

--- client/IFGF-MONTREAL/src/components/Cards.vue
+++ client/IFGF-MONTREAL/src/components/Cards.vue
@@ -1,21 +1,53 @@
 <template>
-<div class="container">
-    <div class="row">
-        <div class="col-md-4">
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and 
-                        make up the bulk of the card's content.
-                    </p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 mb-4">
+          <div class="card" style="width: 18rem;">
+            <img src="@/assets/sunday-school.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Sunday School</h5>
+              <p class="card-text">Teens Ministry</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
+          </div>
         </div>
+  
+        <div class="col-md-6 mb-4">
+          <div class="card" style="width: 18rem;">
+            <img src="@/assets/adults-icare.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Adults iCare</h5>
+              <p class="card-text">Teens Ministry</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+  
+        <div class="col-md-6 mb-4">
+          <div class="card" style="width: 18rem;">
+            <img src="@/assets/teens-ministry.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Teens Ministry</h5>
+              <p class="card-text">Teens Ministry</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+  
+        <div class="col-md-6 mb-4">
+          <div class="card" style="width: 18rem;">
+            <img src="@/assets/prayer-ministry.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Prayer Warriors</h5>
+              <p class="card-text">Teens Ministry</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-</template>
+  </template>
+  
 
 <script>
 export default{
