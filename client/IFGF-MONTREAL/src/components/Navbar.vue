048c34f28232fc4a8fce2a3c5db06d4879e4d5dc
Mahanaim Rubin Yo <108913637+AyoMahan@users.noreply.github.com>
2025-02-26 00:05:11 -0500

    Cards ministry init

--- client/IFGF-MONTREAL/src/components/Navbar.vue
+++ client/IFGF-MONTREAL/src/components/Navbar.vue
@@ -10,7 +10,10 @@
               <RouterLink class="nav-link" to="/about">About</RouterLink>
             </li>
             <li class="nav-item">
-              <RouterLink class="nav-link" to="/about">Reach Out</RouterLink>
+              <RouterLink class="nav-link" to="/events">Reach Out</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/events">Connect</RouterLink>
             </li>
             <li class="nav-item">
               <RouterLink class="nav-link" to="/about">Login</RouterLink>
