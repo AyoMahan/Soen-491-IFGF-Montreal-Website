7eee7f4a5a59d7f827731824ecc6296ea5a4f305
Mahanaim Rubin Yo <108913637+AyoMahan@users.noreply.github.com>
2025-02-28 00:54:06 -0500

    updated form #8 #20
    Better take and export information to later process in email

--- client/IFGF-MONTREAL/src/components/Form.vue
+++ client/IFGF-MONTREAL/src/components/Form.vue
@@ -1,56 +1,105 @@
 <template>
-<form class="row g-3">
-  <div class="col-md-6">
-    <label for="inputEmail4" class="form-label">Name</label>
-    <input type="email" class="form-control" id="inputEmail4">
-  </div>
-  <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">Email</label>
-    <input type="password" class="form-control" id="inputPassword4">
-  </div>
-  <div class="col-12">
-    <label for="inputAddress" class="form-label">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="col-md-6">
-    <label for="inputCity" class="form-label">Phone Number</label>
-    <input type="text" class="form-control" id="inputCity">
-  </div>
-  <div class="col-md-4">
-    <label for="inputState" class="form-label">Care Group</label>
-    <select id="inputState" class="form-select">
-      <option selected>Choose...</option>
-      <option>Adult Icare</option>
-      <option>Teens Icare</option>
-      <option>Sunday School</option>
-      <option>Prayer Ministry</option>
-    </select>
-  </div>
-  <div class="col-md-2">
-    <label for="inputZip" class="form-label">Age</label>
-    <input type="text" class="form-control" id="inputZip">
-  </div>
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Reply to me
-      </label>
+  <form class="row g-3" @submit.prevent="sendEmail">
+    <div class="col-md-6">
+      <label for="inputName" class="form-label">Name</label>
+      <input type="text" class="form-control" id="inputName" v-model="formData.name" required>
     </div>
-  </div>
-  <div class="input-group">
-    <span class="input-group-text">Tell us about yourself</span>
-    <textarea class="form-control" aria-label="With textarea"></textarea>
-</div>
-  <div class="col-12">
-    <button type="submit" class="btn btn-primary">Sign in</button>
-  </div>
-</form>
-
+    <div class="col-md-6">
+      <label for="inputEmail" class="form-label">Email</label>
+      <input type="email" class="form-control" id="inputEmail" v-model="formData.email" required>
+    </div>
+    <div class="col-12">
+      <label for="inputAddress" class="form-label">Address</label>
+      <input type="text" class="form-control" id="inputAddress" v-model="formData.address">
+    </div>
+    <div class="col-md-6">
+      <label for="inputPhone" class="form-label">Phone Number</label>
+      <input type="text" class="form-control" id="inputPhone" v-model="formData.phone">
+    </div>
+    <div class="col-md-4">
+      <label for="inputCareGroup" class="form-label">Care Group</label>
+      <select id="inputCareGroup" class="form-select" v-model="formData.careGroup">
+        <option disabled value="">Choose...</option>
+        <option>Adult Icare</option>
+        <option>Teens Icare</option>
+        <option>Sunday School</option>
+        <option>Prayer Ministry</option>
+      </select>
+    </div>
+    <div class="col-md-2">
+      <label for="inputAge" class="form-label">Age</label>
+      <input type="text" class="form-control" id="inputAge" v-model="formData.age">
+    </div>
+    <div class="col-12">
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="gridCheck" v-model="formData.reply">
+        <label class="form-check-label" for="gridCheck">
+          Reply to me
+        </label>
+      </div>
+    </div>
+    <div class="input-group">
+      <span class="input-group-text">Tell us about yourself</span>
+      <textarea class="form-control" v-model="formData.message"></textarea>
+    </div>
+    <div class="col-12">
+      <button type="submit" class="btn btn-primary">Send</button>
+    </div>
+  </form>
 </template>
 
 <script>
-export default{
-    name: "Form",
-}
+export default {
+  name: "Form",
+  data() {
+    return {
+      formData: {
+        name: "",
+        email: "",
+        address: "",
+        phone: "",
+        careGroup: "",
+        age: "",
+        reply: false,
+        message: "",
+      }
+    };
+  },
+  methods: {
+    async sendEmail() {
+      try {
+        const response = await fetch("http://localhost:5000/send-email", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(this.formData),
+        });
+
+        const result = await response.json();
+        if (response.ok) {
+          alert("Email sent successfully!");
+          this.resetForm();
+        } else {
+          alert("Error: " + result.error);
+        }
+      } catch (error) {
+        alert("Failed to send email.");
+        console.error(error);
+      }
+    },
+    resetForm() {
+      this.formData = {
+        name: "",
+        email: "",
+        address: "",
+        phone: "",
+        careGroup: "",
+        age: "",
+        reply: false,
+        message: "",
+      };
+    }
+  }
+};
 </script>
