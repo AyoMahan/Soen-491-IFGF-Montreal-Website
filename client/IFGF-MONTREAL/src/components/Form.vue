35992836f9570caf101d512554820f66ad711255
Mahanaim Rubin Yo <108913637+AyoMahan@users.noreply.github.com>
2025-03-04 13:39:23 -0500

    form fix #17

--- client/IFGF-MONTREAL/src/components/Form.vue
+++ client/IFGF-MONTREAL/src/components/Form.vue
@@ -1,51 +1,60 @@
 <template>
-  <form class="row g-3" @submit.prevent="sendEmail">
-    <div class="col-md-6">
-      <label for="inputName" class="form-label">Name</label>
-      <input type="text" class="form-control" id="inputName" v-model="formData.name" required>
-    </div>
-    <div class="col-md-6">
-      <label for="inputEmail" class="form-label">Email</label>
-      <input type="email" class="form-control" id="inputEmail" v-model="formData.email" required>
-    </div>
-    <div class="col-12">
-      <label for="inputAddress" class="form-label">Address</label>
-      <input type="text" class="form-control" id="inputAddress" v-model="formData.address">
-    </div>
-    <div class="col-md-6">
-      <label for="inputPhone" class="form-label">Phone Number</label>
-      <input type="text" class="form-control" id="inputPhone" v-model="formData.phone">
-    </div>
-    <div class="col-md-4">
-      <label for="inputCareGroup" class="form-label">Care Group</label>
-      <select id="inputCareGroup" class="form-select" v-model="formData.careGroup">
-        <option disabled value="">Choose...</option>
-        <option>Adult Icare</option>
-        <option>Teens Icare</option>
-        <option>Sunday School</option>
-        <option>Prayer Ministry</option>
-      </select>
-    </div>
-    <div class="col-md-2">
-      <label for="inputAge" class="form-label">Age</label>
-      <input type="text" class="form-control" id="inputAge" v-model="formData.age">
-    </div>
-    <div class="col-12">
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" id="gridCheck" v-model="formData.reply">
-        <label class="form-check-label" for="gridCheck">
-          Reply to me
-        </label>
+  <div class="form-container">
+    <h2 class="form-title">Get in Touch</h2>
+    <form class="row g-3" @submit.prevent="sendEmail">
+      <div class="col-md-6">
+        <label for="inputName" class="form-label">Name</label>
+        <input type="text" class="form-control" id="inputName" v-model="formData.name" required>
       </div>
-    </div>
-    <div class="input-group">
-      <span class="input-group-text">Tell us about yourself</span>
-      <textarea class="form-control" v-model="formData.message"></textarea>
-    </div>
-    <div class="col-12">
-      <button type="submit" class="btn btn-primary">Send</button>
-    </div>
-  </form>
+
+      <div class="col-md-6">
+        <label for="inputEmail" class="form-label">Email</label>
+        <input type="email" class="form-control" id="inputEmail" v-model="formData.email" required>
+      </div>
+
+      <div class="col-12">
+        <label for="inputAddress" class="form-label">Address</label>
+        <input type="text" class="form-control" id="inputAddress" v-model="formData.address">
+      </div>
+
+      <div class="col-md-6">
+        <label for="inputPhone" class="form-label">Phone Number</label>
+        <input type="text" class="form-control" id="inputPhone" v-model="formData.phone">
+      </div>
+
+      <div class="col-md-4">
+        <label for="inputCareGroup" class="form-label">Care Group</label>
+        <select id="inputCareGroup" class="form-select" v-model="formData.careGroup">
+          <option disabled value="">Choose...</option>
+          <option>Adult Icare</option>
+          <option>Teens Icare</option>
+          <option>Sunday School</option>
+          <option>Prayer Ministry</option>
+        </select>
+      </div>
+
+      <div class="col-md-2">
+        <label for="inputAge" class="form-label">Age</label>
+        <input type="text" class="form-control" id="inputAge" v-model="formData.age">
+      </div>
+
+      <div class="col-12">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="gridCheck" v-model="formData.reply">
+          <label class="form-check-label" for="gridCheck">Reply to me</label>
+        </div>
+      </div>
+
+      <div class="col-12">
+        <label for="message" class="form-label">Tell us about yourself</label>
+        <textarea class="form-control" id="message" v-model="formData.message" rows="4"></textarea>
+      </div>
+
+      <div class="col-12 text-center">
+        <button type="submit" class="btn btn-custom">Send</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -103,3 +112,5 @@ export default {
   }
 };
 </script>
+
+
