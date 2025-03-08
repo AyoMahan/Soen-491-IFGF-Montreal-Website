2eba705201c408493a0196cf3fce25fc227aa6b1
Mahanaim Rubin Yo <108913637+AyoMahan@users.noreply.github.com>
2025-03-07 19:05:00 -0500

    Docker containerization

--- client/IFGF-MONTREAL/vite.config.js
+++ client/IFGF-MONTREAL/vite.config.js
@@ -17,4 +17,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0',  // Allow external access to the server
+    port: 5173         // Ensure Vite is running on port 5173
+  }  
 })
